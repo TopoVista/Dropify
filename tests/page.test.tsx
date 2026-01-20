b851ffe6bf3b3253195944cf324731d5664bd6ff
Souradeep Ghosh <souradeepghosh608@gmail.com>
2026-01-20 19:36:36 +0530

    Delete tests directory

--- tests/page.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Page from '../app/page'
-
-test('renders hello world', () => {
-  render(<Page />)
-  expect(screen.getByText('Hello Dropify')).toBeInTheDocument()
-})
