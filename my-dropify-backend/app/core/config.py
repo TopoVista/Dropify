ecb4f039f775072c71efe062b3ecf4c8ba84ee27
souradeep <souradeepghosh608@gmail.com>
2026-02-15 17:25:29 +0530

    after vercel part 3

--- my-dropify-backend/app/core/config.py
+++ my-dropify-backend/app/core/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3001")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://dropify_db_user:xsK9zv9fUs8U5QlTolmKyXJ7c7YyV5Jb@dpg-d68pg60boq4c73d65qlg-a/dropify_db")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "https://dropify-frontend-2ngdo9rx9-topovistas-projects.vercel.app")
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 SESSION_TTL_SECONDS = int(os.getenv("SESSION_TTL_SECONDS", 3600))
 
